95d93fab8c3d86e0d16f1f686f585dc849ec05ae
Razin Shaikh <razinshaikh@outlook.com>
2021-12-15 15:24:53 +0000

    unit_normal_regularization_factor in params

--- vae_train.py
+++ vae_train.py
@@ -33,7 +33,7 @@ params = {
     'use_labels_in_encoder': True,
     'gaussians_mean_init': (-1., 1.),
     'gaussians_log_var_init': (0.7, 0.),
-    'if_regularize_unit_normal': False,
+    'unit_normal_regularization_factor': 0.1, # set to 0 if you don't want to regularize
     'beta': 1
 }
 
