a775788c1b350d0ab3579aa31cf5bbb4f1d71e3a
Razin Shaikh <razinshaikh@outlook.com>
2022-01-24 12:48:27 +0000

    added 'any' labels in the encoding dictionary

--- vae/encoding_dictionary.py
+++ vae/encoding_dictionary.py
@@ -2,24 +2,28 @@ concept_domains = ['colour', 'size', 'shape', 'position']
 
 enc_dict = {
     'colour' : {
+        'any': -1,
         'blue': 0,
         'green': 1,
         'red': 2,
     },
 
     'size' : {
+        'any': -1,
         'small': 0,
         'medium': 1,
         'large': 2,
     },
 
     'shape' : {
+        'any': -1,
         'triangle': 0,
         'square': 1,
         'circle': 2,
     },
 
     'position' : {
+        'any': -1,
         'top': 0,
         'centre': 1,
         'bottom': 2,
@@ -28,24 +32,28 @@ enc_dict = {
 
 dec_dict = {
     'colour' : {
+        -1: 'any',
         0 : 'blue',
         1 : 'green',
         2 : 'red',
     },
 
     'size' : {
+        -1: 'any',
         0 : 'small',
         1 : 'medium',
         2 : 'large',
     },
 
     'shape' : {
+        -1: 'any',
         0 : 'triangle',
         1 : 'square',
         2 : 'circle',
     },
 
     'position' : {
+        -1: 'any',
         0 : 'top',
         1 : 'centre',
         2 : 'bottom',
